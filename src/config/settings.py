a245431b17f32487ddae65cdad094801d5c12641
xborismenx <boris.tikhonov.21@gmail.com>
2025-05-14 20:33:24 +0300

    added import to __init__ file & changed name base settings

--- src/config/settings.py
+++ src/config/settings.py
@@ -3,6 +3,6 @@ from pathlib import Path
 from pydantic_settings import BaseSettings
 
 
-class Settings(BaseSettings):
+class BaseAppSettings(BaseSettings):
     BASE_DIR: Path = Path(__file__).parent.parent
     PATH_TO_DB: str = str(BASE_DIR / "database" / "source" / "data.db")
