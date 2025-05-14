41347bbfecc9c06eaf3f6d2d127814628ee8d9b8
xborismenx <boris.tikhonov.21@gmail.com>
2025-05-14 22:04:30 +0300

    added variables to connect postgres to settings

--- src/config/settings.py
+++ src/config/settings.py
@@ -1,8 +1,19 @@
+import os
 from pathlib import Path
 
+from dotenv import load_dotenv
 from pydantic_settings import BaseSettings
+
+load_dotenv()
 
 
 class BaseAppSettings(BaseSettings):
     BASE_DIR: Path = Path(__file__).parent.parent
     PATH_TO_DB: str = str(BASE_DIR / "database" / "source" / "data.db")
+    """POSTGRES DATABASE"""
+    POSTGRES_DB: str = os.getenv("POSTGRES_DB")
+    POSTGRES_USER: str = os.getenv("POSTGRES_USER")
+    POSTGRES_PASSWORD: str = os.getenv("POSTGRES_PASSWORD")
+    POSTGRES_HOST: str = os.getenv("POSTGRES_HOST")
+    POSTGRES_PORT: str = os.getenv("POSTGRES_PORT")
+
