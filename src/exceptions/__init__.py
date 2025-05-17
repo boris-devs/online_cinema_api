d098681701df2ea37b05f1e3d2516953f0d696bc
xborismenx <boris.tikhonov.21@gmail.com>
2025-05-17 18:48:24 +0300

    Add exception imports to exceptions module
    This change centralizes the imports of specific exception classes from their respective submodules. It improves code organization and makes the exceptions module more convenient to use across the project.

---
+++ src/exceptions/__init__.py
@@ -0,0 +1,3 @@
+from src.exceptions.security import TokenExpiredError, InvalidTokenError
+from src.exceptions.email import BaseEmailError
+from src.exceptions.storage import S3ConnectionError, S3FileUploadError
