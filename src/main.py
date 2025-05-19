6e2265e6120bdf5bcfdf0698d612e78562dab044
xborismenx <boris.tikhonov.21@gmail.com>
2025-05-20 01:13:17 +0300

    Add movies_router to FastAPI application
    Integrated movies_router into the FastAPI app with a defined prefix and tag. This addition expands the API to include movie-related endpoints, enhancing functionality.

--- src/main.py
+++ src/main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
 
-from routes import accounts_router
+from routes import accounts_router, movies_router
 app = FastAPI()
 
 
 api_prefix = "/api/"
 
 app.include_router(accounts_router, prefix=f"{api_prefix}/accounts", tags=["accounts"])
+app.include_router(movies_router, prefix=f"{api_prefix}/movies", tags=["movies"])
