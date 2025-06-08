ad5222307c414b4b5e45ad75477b7ac5ed2fe00f
xborismenx <boris.tikhonov.21@gmail.com>
2025-06-08 21:48:19 +0300

    Refactor movie schemas and add comment-related models
    Refactored schemas to enhance structure and consistency, including normalization for list fields and use of `from_attributes` in multiple models. Introduced new schemas for movie comments and user reactions to support comment-related functionality. Improved field validation and type annotations for increased robustness.

--- src/schemas/movies.py
+++ src/schemas/movies.py
@@ -1,26 +1,32 @@
-from typing import Optional
+from typing import Optional, List
 
-from pydantic import BaseModel
-
+from pydantic import BaseModel, field_validator, Field
+import uuid
 
 class StarsSchema(BaseModel):
     id: int
     name: str
 
+    class Config:
+        from_attributes = True
 
 class GenresSchema(BaseModel):
     id: int
     name: str
+
+    class Config:
+        from_attributes = True
 
 
 class DirectorsSchema(BaseModel):
     id: int
     name: str
 
+    class Config:
+        from_attributes = True
+
 
 class MovieBaseSchema(BaseModel):
-    id: int
-    uuid: str
     name: str
     year: int
     time: int
@@ -31,13 +37,53 @@ class MovieBaseSchema(BaseModel):
     description: Optional[str]
     price: Optional[float]
     certification_id: int
-    stars: list[StarsSchema]
-    genres: list[GenresSchema]
-    directors: list[DirectorsSchema]
+    stars: List[str]
+    genres: List[str]
+    directors: List[str]
 
 
 class MovieListSchema(MovieBaseSchema):
-    pass
+    id: int
+
 
 class MovieDetailSchema(MovieBaseSchema):
+    id: int
+    uuid: uuid.UUID
+    stars: List[StarsSchema]
+    genres: List[GenresSchema]
+    directors: List[DirectorsSchema]
+
+    class Config:
+        from_attributes = True
+
+class MovieCreateSchema(MovieBaseSchema):
     pass
+
+    @field_validator("stars", "genres", "directors", mode="before")
+    @classmethod
+    def normalize_list_fields(cls, value: List[str]) -> List[str]:
+        return [item.title() for item in value]
+
+
+class MovieCommentBaseSchema(BaseModel):
+    text: str
+    user_profile_id: int
+    movie_id: int
+
+class MovieCommentCreateRequestSchema(BaseModel):
+    text: str = Field(min_length=1, max_length=100)
+
+    class Config:
+        from_attributes = True
+
+class MovieCommentCreateResponseSchema(MovieCommentBaseSchema):
+    id: int
+
+    class Config:
+        from_attributes = True
+
+class MovieUserReactionResponseModel(BaseModel):
+    message: str
+
+    class Config:
+        from_attributes = True
