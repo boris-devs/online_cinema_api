7e1d817bf3c12f5fd4b91ab273329037373419c3
xborismenx <boris.tikhonov.21@gmail.com>
2025-06-08 21:52:47 +0300

    Update API prefixes and rename movies route to catalog
    Removed the trailing slash from `api_prefix` for consistency and renamed the `movies` route to `catalog` to better reflect its content. These changes improve clarity and align with API naming conventions.

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ from routes import accounts_router, movies_router
 app = FastAPI()
 
 
-api_prefix = "/api/"
+api_prefix = "/api"
 
 app.include_router(accounts_router, prefix=f"{api_prefix}/accounts", tags=["accounts"])
-app.include_router(movies_router, prefix=f"{api_prefix}/movies", tags=["movies"])
+app.include_router(movies_router, prefix=f"{api_prefix}/catalog", tags=["catalog"])
