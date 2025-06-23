540b31258ca74fd3b5b11d5d64515f05a83c9423
xborismenx <boris.tikhonov.21@gmail.com>
2025-06-23 16:57:39 +0300

    include router shopping_cart to app

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 
-from routes import accounts_router, movies_router
+from routes import accounts_router, movies_router, shopping_cart_router
 app = FastAPI()
 
 
@@ -8,3 +8,4 @@ api_prefix = "/api"
 
 app.include_router(accounts_router, prefix=f"{api_prefix}/accounts", tags=["accounts"])
 app.include_router(movies_router, prefix=f"{api_prefix}/catalog", tags=["catalog"])
+app.include_router(shopping_cart_router, prefix=f"{api_prefix}/cart", tags=["cart"])
--- src/routes/__init__.py
+++ src/routes/__init__.py
@@ -1,2 +1,3 @@
 from routes.accounts import router as accounts_router
 from routes.movies import router as movies_router
+from routes.shopping_cart import router as shopping_cart_router
